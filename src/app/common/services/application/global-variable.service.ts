bd02a11d7772c7a4f4040e6c35a299c5d0194466
강효선 <kanghyoseon@ganghyoseon-ui-MacBook-Pro.local>
2021-05-12 00:14:19 +0900

    modify qeustion

--- src/app/common/services/application/global-variable.service.ts
+++ src/app/common/services/application/global-variable.service.ts
@@ -18,15 +18,9 @@ export class GlobalVariableService {
     // TODO: question http client를 사용하여 assets/config/configuration.json 파일을 로드하여 저장할 것.
     // 단, return type 은 Promise여야 함.
     getConfiguration(): Promise<any> {
-        return this.http.get('assets/config/configuration.json')
-            .toPromise()
-            .then((res: any) => {
-                if (console && console.log) {
-                    console.log('configuration : ', res);
-                }
-                this.remoteUrl = res.remote;
-                return res;
-            });
+        return new Promise((resolve, reject) => {
+            resolve([]);
+        });
     }
 
 }
--- src/app/common/services/services.module.ts
+++ src/app/common/services/services.module.ts
@@ -6,7 +6,7 @@ import { SharedModule } from '../modules/shared.module';
 // TODO: question app 이 구동되기전에 assets/config/configuration.json 을 가져와 저장 후 app이 구동이 되도록 완성하시오.
 // 최초 app을 구동하기 위한 설정정보를 가져오는 함수.
 export function getConfiguration(globalService: GlobalVariableService): any {
-  return () => globalService.getConfiguration();
+  return null;
 }
 
 @NgModule({
